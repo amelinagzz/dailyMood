ab87f78751bc6a4b798ad300c68da795d377b59f
Adriana Gonzalez <adriana.gonzalez@ipointsystems.com>
2017-01-11 18:13:31 -0600

    Initial Commit

---
+++ dailymood WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  dailymood WatchKit Extension
+//
+//  Created by Adriana Gonzalez on 1/11/17.
+//  Copyright © 2017 AMGM. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ dailymood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dailymood
+//
+//  Created by Adriana Gonzalez on 1/11/17.
+//  Copyright © 2017 AMGM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
